cedb675a3389017e3631385593f451739ed539cf
wuberde <wuberde@gmail.com>
2021-01-15 00:59:42 +0100

    basic design of login/signUp pages --pei-ling

--- pages/screens/Login.js
+++ pages/screens/Login.js
@@ -1,67 +1,124 @@
-import React from 'react'
-import { StyleSheet, View, Button, TextInput } from 'react-native'
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
 export default class Login extends React.Component {
   state = {
     email: '',
-    password: ''
-  }
+    password: '',
+  };
 
-  handleEmailChange = email => {
-    this.setState({ email })
-  }
+  handleEmailChange = (email) => {
+    this.setState({ email });
+  };
 
-  handlePasswordChange = password => {
-    this.setState({ password })
-  }
+  handlePasswordChange = (password) => {
+    this.setState({ password });
+  };
 
   onLogin = async () => {
-    const { email, password } = this.state
+    const { email, password } = this.state;
     try {
       if (email.length > 0 && password.length > 0) {
-        this.props.navigation.navigate('App')
+        this.props.navigation.navigate('App');
       }
     } catch (error) {
-      alert(error)
+      alert(error);
     }
-  }
+  };
 
-  goToSignup = () => this.props.navigation.navigate('Signup')
+  goToSignup = () => this.props.navigation.navigate('Signup');
   render() {
-    const { email, password } = this.state
+    const { email, password } = this.state;
 
     return (
       <View style={styles.container}>
-        <View style={{ margin: 10 }}>
+        <View>
+          <Text style={styles.logo}>EasyGoal</Text>
+        </View>
+        <View style={styles.inputView}>
           <TextInput
-            name='email'
+            name="email"
             value={email}
-            placeholder='Enter email'
-            autoCapitalize='none'
+            placeholder="Email/Username..."
+            placeholderTextColor="#4b0082"
+            autoCapitalize="none"
             onChangeText={this.handleEmailChange}
           />
         </View>
-        <View style={{ margin: 10 }}>
+        <View style={styles.inputView}>
           <TextInput
-            name='password'
+            name="password"
             value={password}
-            placeholder='Enter password'
+            placeholder="Enter password"
+            placeholderTextColor="#4b0082"
             secureTextEntry
             onChangeText={this.handlePasswordChange}
           />
         </View>
-        <Button title='Login' onPress={this.onLogin} />
-        <Button title='Go to Signup' onPress={this.goToSignup} />
+        <View>
+          <TouchableOpacity>
+            <Text style={styles.forgot}>Forgot Password?</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ width: '80%' }}>
+          <TouchableOpacity style={styles.loginBtn}>
+            <Button
+              style={styles.loginText}
+              title="Login"
+              onPress={this.onLogin}
+            />
+          </TouchableOpacity>
+          <Button title="Go to Signup" onPress={this.goToSignup} />
+        </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7ac8e4',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    justifyContent: 'center',
+  },
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: '#4b0082',
+    marginBottom: 40,
+  },
+  inputView: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 10,
+  },
+  forgot: {
+    marginBottom: 10,
+    color: '#4b0082',
+    fontSize: 14,
+  },
+  loginBtn: {
+    backgroundColor: '#fcbc84',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+});
+
+// dark purple #4b0082
--- pages/screens/Signup.js
+++ pages/screens/Signup.js
@@ -1,23 +1,140 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
 export default class Signup extends React.Component {
-  goToLogin = () => this.props.navigation.navigate('Login')
+  state = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  handleEmailChange = (email) => {
+    this.setState({ email });
+  };
+
+  handlePasswordChange = (password) => {
+    this.setState({ password });
+  };
+
+  handleConfirmPasswordChange = (confirmPassword) => {
+    this.setState({ confirmPassword });
+  };
+
+  onSignup = async () => {
+    const { email, password, confirmPassword } = this.state;
+    try {
+      if (
+        email.length > 0 &&
+        password.length > 0 &&
+        confirmPassword.length > 0
+      ) {
+        this.props.navigation.navigate('App');
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  signupOrBackToLogIn = () => this.props.navigation.navigate('Login');
   render() {
+    const { email, password, confirmPassword } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Signup</Text>
-        <Button title='Go to Login' onPress={this.goToLogin} />
+        <View>
+          <Text style={styles.logo}>Sign Up</Text>
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            name="email"
+            value={email}
+            placeholder="Email/Username..."
+            placeholderTextColor="#4b0082"
+            autoCapitalize="none"
+            onChangeText={this.handleEmailChange}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            name="password"
+            value={password}
+            placeholder="Enter password"
+            placeholderTextColor="#4b0082"
+            secureTextEntry
+            onChangeText={this.handlePasswordChange}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            name="confirmPassword"
+            value={confirmPassword}
+            placeholder="Confirm password"
+            placeholderTextColor="#4b0082"
+            secureTextEntry
+            onChangeText={this.handleConfirmPasswordChange}
+          />
+        </View>
+        <View style={{ width: '80%' }}>
+          <TouchableOpacity style={styles.loginBtn}>
+            <Button
+              style={styles.loginText}
+              title="Sign up"
+              onPress={this.onSignup}
+            />
+          </TouchableOpacity>
+        </View>
+        <Button
+          style={styles.loginText}
+          title="Go to Login"
+          onPress={this.signupOrBackToLogIn}
+        />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7ac8e4',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    justifyContent: 'center',
+  },
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: '#4b0082',
+    marginBottom: 40,
+  },
+  inputView: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 10,
+  },
+  forgot: {
+    marginBottom: 10,
+    color: '#4b0082',
+    fontSize: 14,
+  },
+  loginBtn: {
+    backgroundColor: '#fcbc84',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+});
